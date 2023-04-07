e1ddc8bcad19823a66b3bf95318b26a0f0255da1
SeraphF2P <110803319+SeraphF2P@users.noreply.github.com>
2023-04-07 23:49:16 +0300

    upload

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,13 +6,13 @@ import GoogleProvider from 'next-auth/providers/google'
 function getGoogleCredentials(): { clientId: string; clientSecret: string } {
   const clientId = process.env.GOOGLE_CLIENT_ID
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET
-  // if (!clientId || clientId.length === 0) {
-  //   throw new Error('Missing GOOGLE_CLIENT_ID')
-  // }
+  if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_ID')
+  }
 
-  // if (!clientSecret || clientSecret.length === 0) {
-  //   throw new Error('Missing GOOGLE_CLIENT_SECRET')
-  // }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_SECRET')
+  }
   return { clientId, clientSecret }
 }
 
